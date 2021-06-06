ff68aa009ce16724fb252298a3792654c365549d
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-06 16:01:42 -0400

    npm update

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 console.log(process.env.MONGODB_URI) 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-gaia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kalibfinalproject', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
