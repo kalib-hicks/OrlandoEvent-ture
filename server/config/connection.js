7e18886c120aa9bd5c9b83ddb53a4bd828d028d4
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-06 16:07:16 -0400

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 console.log(process.env.MONGODB_URI) 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kalibfinalproject', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kalib-hicks:Bard1218958@cluster0.sspdg.mongodb.net/kalibfinalproject?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
