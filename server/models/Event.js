0dd9e44c8eb3a63a99980fe3512a790860bb9450
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-06 16:46:09 -0400

    updates

--- server/models/Event.js
+++ server/models/Event.js
@@ -29,18 +29,7 @@ const eventSchema = new Schema(
       type: String,
       required: true
     },
-    city: {
-      type: String,
-      required: true
-    },
-    state: {
-      type: String,
-      required: true
-    },
-    zip: {
-      type: String,
-      required: true
-    },
+  
     description: {
       type: String,
       required: true
