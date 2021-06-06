847b5730ccba27dd4f8f15b05ebcfb74a13c620b
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-05 21:08:40 -0400

    merge main

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://orlandoevent-ture.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
