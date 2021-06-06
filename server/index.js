ea5fcfd97b2ca1eb02ef2426d6e8f26e0a818f52
Kalib Hicks <kalib.hicks@gmail.com>
2021-06-05 21:03:45 -0400

    merge

--- server/index.js
+++ server/index.js
@@ -8,13 +8,17 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+app.get('/', (req, res) => {
+    res.send('Hello to mem api');
+})
+
+const CONNECTION_URL = 'mongodb+srv://kalib-hicks:Bard1218958@cluster0.sspdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
